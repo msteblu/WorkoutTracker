9b7f43111cde5620cac0b45bbdb3e3f5c289f540
Megan Steblay <megan@steblay.net>
2021-08-18 16:11:42 -0500

    Fixing Exercise model

--- models/Exercise.js
+++ models/Exercise.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ExerciseSchema = new Schema({
-  _id: Schema.Types.ObjectId,
   type: String,
   name: String,
   duration: Number,
