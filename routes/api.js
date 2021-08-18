e46dbedac394ff3cb4b570cd30b95a3a4c4d3966
Megan Steblay <megan@steblay.net>
2021-08-18 16:13:12 -0500

    Fixing initial routes in api.js

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,16 @@
 const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 const Exercise = require("../models/Exercise.js");
+const mongojs = require("mongojs");
 
 router.get("/api/workouts", (req, res) => {
-  Workout.find({})
-    // .populate("exercises")
-    .exec(function (err, workouts) {
-      if (!err) {
-        res.json(workouts);
-        console.log(workouts);
-      } else {
-        console.log(err);
-      }
-    });
+  Workout.find({}).exec(function (err, workouts) {
+    if (!err) {
+      res.json(workouts);
+    } else {
+      console.log(err);
+    }
+  });
 });
 
 router.post("/api/workouts", ({ body }, res) => {
@@ -22,6 +20,36 @@ router.post("/api/workouts", ({ body }, res) => {
     })
     .catch((err) => {
       res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Exercise.create(req.body)
+    .then(({ _id }) =>
+      Workout.findOneAndUpdate(
+        { _id: mongojs.ObjectId(req.params.id) },
+        // {},
+        { $push: { exercises: _id } },
+        { new: true }
+      )
+    )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    // .populate("exercises")
+    .exec(function (err, workouts) {
+      if (!err) {
+        res.json(workouts);
+      } else {
+        console.log(err);
+      }
     });
 });
 
